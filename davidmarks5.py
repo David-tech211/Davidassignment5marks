0859863d8372db020319f36c5d81234dbde7e560
David-tech <david-efe-zino.erue@binghamuni.edu.ng>
2024-12-04 21:30:50 -0800

    My first commit

---
+++ davidmarks5.py
@@ -0,0 +1,11 @@
+number_list = [22,32,44,12,45,66,76,81,10,90]
+
+index = 0
+while number_list[index:]:
+    index+= 1
+
+    number_index = index - 1
+
+for number in range(number_index, -1, -1):
+
+    print(number_list[number])
